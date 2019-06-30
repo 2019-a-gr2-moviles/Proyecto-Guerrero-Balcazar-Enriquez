97bd6a6f4de88ce49bc555ffaea1fd19685104d1
Fabio <fabio.and1514@gmail.com>
2019-06-29 23:45:37 -0500

    Diseño de Login Terminado

--- Projecto_Libreria/app/src/main/java/com/example/proyecto_libreria/MainActivity.kt
+++ Projecto_Libreria/app/src/main/java/com/example/proyecto_libreria/MainActivity.kt
@@ -16,20 +16,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         identificarIdioma()
-        btn_maps.setOnClickListener {
-            irIntentRespuesta()
+
+        login_btn_ingresar.setOnClickListener {
+            irACatalogoActivity()
         }
 
 
     }
-    fun irIntentRespuesta(){
+
+    fun irACatalogoActivity() {
         val intent= Intent(
-            this, LoginActivity::class.java
+            this, CatalogoActivity::class.java
         )
-
         startActivity(intent);
-
     }
+
 
     fun identificarIdioma() {
         val languageIdentifier: FirebaseLanguageIdentification = FirebaseNaturalLanguage.getInstance().languageIdentification
